43f6ff115149a91dc7e73e0af54142a4920e2e86
Bharat Dua <bharatdua13@gmail.com>
2024-04-16 15:04:57 +0530

    Basic layout structure of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+import { Box } from "@mui/material";
+import "./App.css";
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width={"400px"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
-export default App
+export default App;
