8137229092713dd948c2377bb6db549ffb7d45f9
Bharat Dua <bharatdua13@gmail.com>
2024-04-17 14:25:32 +0530

    done the SearchExercise component with search exercise result and pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import { fetchData, exerciseOptions } from "../utiles/FetchData";
+import ExerciseCard from "./ExerciseCard";
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisePerPage = 9;
+  console.log(exercises);
+
+  const indexOfLastExercise = currentPage * exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1000, behavior: "smooth" });
+  };
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb={"46px"}>
+        exercise result
+      </Typography>
+      <Stack
+        direction={"row"}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+      >
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt={"100px"} alignItems={"center"}>
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
